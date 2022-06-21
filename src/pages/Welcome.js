5af6bfe63c93f53855373bc9a7a78217d363fdd6
DesperateHiking <perevechenkov_1999@mail.ru>
2022-06-21 13:20:39 +0500

    nested routing

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,3 +1,12 @@
+import { Route } from 'react-router-dom';
+
 export default function Welcome(props) {
-  return <h1>Welcome</h1>;
+  return (
+    <section>
+      <h1>Welcome</h1>
+      <Route path='/welcome/new-user'>
+        <p>New User</p>
+      </Route>
+    </section>
+  );
 }
