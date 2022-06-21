b7be638334f94e6a179898b08322c27220bf0d11
DesperateHiking <perevechenkov_1999@mail.ru>
2022-06-21 13:31:43 +0500

    emptying project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router>
-    <App />
-  </Router>
-);
+root.render(<App />);
