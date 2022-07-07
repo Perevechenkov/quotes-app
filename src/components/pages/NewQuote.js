845bd3e4baf8604f978267f1d6be94b2e9d54dbc
DesperateHiking <perevechenkov_1999@mail.ru>
2022-07-07 22:46:07 +0500

    redirectiong after form submit via hostory hook

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,5 +1,15 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../quotes/QuoteForm';
 
 export default function NewQuote(props) {
-  return <QuoteForm />;
+  const history = useHistory();
+
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData);
+
+    history.push('/quotes');
+  };
+
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 }
--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,17 +1,37 @@
 import classes from './QuoteForm.module.scss';
 import Card from '../UI/Card';
+import { useRef } from 'react';
 
 export default function QuoteForm(props) {
+  const authorRef = useRef();
+  const textRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const author = authorRef.current.value;
+    const text = textRef.current.value;
+
+    if (author.trim().length === 0 && text.trim().length === 0) {
+      return;
+    }
+
+    props.onAddQuote({
+      author,
+      text,
+    });
+  };
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='author'>Author</label>
-          <input type='text' id='author'></input>
+          <input type='text' id='author' ref={authorRef}></input>
         </div>
         <div className={classes.control}>
           <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5'></textarea>
+          <textarea id='text' rows='5' ref={textRef}></textarea>
         </div>
         <div className={classes.actions}>
           <button className='btn'>Add Quote</button>
