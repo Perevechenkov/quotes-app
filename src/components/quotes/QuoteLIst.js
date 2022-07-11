6c498ff07e6bc549f6bc172e5e9e632a6a23e80c
DesperateHiking <perevechenkov_1999@mail.ru>
2022-07-11 19:02:39 +0500

    sorting quotes

--- src/components/quotes/QuoteLIst.js
+++ src/components/quotes/QuoteLIst.js
@@ -1,12 +1,41 @@
+import { useHistory, useLocation } from 'react-router-dom';
+
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.scss';
 
+const getSortedQuotes = (quotes, isAscending) => {
+  return quotes.sort((a, b) => {
+    if (isAscending) {
+      return a.id > b.id ? 1 : -1;
+    } else {
+      return a.id < b.id ? 1 : -1;
+    }
+  });
+};
+
 export default function QuoteList(props) {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+  };
+
   return (
-    <ul className={classes.list}>
-      {props.quotes.map(quote => (
-        <QuoteItem key={quote.id} quoteObj={quote} />
-      ))}
-    </ul>
+    <>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
+      <ul className={classes.list}>
+        {getSortedQuotes(props.quotes, isSortingAscending).map(quote => (
+          <QuoteItem key={quote.id} quoteObj={quote} />
+        ))}
+      </ul>
+    </>
   );
 }
