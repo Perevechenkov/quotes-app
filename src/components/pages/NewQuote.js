e2651a69dfbe0430cd547bfca251fc952a04a50e
DesperateHiking <perevechenkov_1999@mail.ru>
2022-07-13 19:22:15 +0500

    adding quotes to firebase

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,13 +1,33 @@
 import { useHistory } from 'react-router-dom';
+import useHttp from '../../hooks/use-http';
 
 import QuoteForm from '../quotes/QuoteForm';
 
+const addQuote = async quoteData => {
+  const response = await fetch(
+    'https://quotes-dummy-db-default-rtdb.europe-west1.firebasedatabase.app/quotes.json',
+    {
+      method: 'POST',
+      body: JSON.stringify(quoteData),
+      headers: {
+        'Content-Type': 'applicaton/json',
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+};
+
 export default function NewQuote(props) {
   const history = useHistory();
+  const { sendRequest, httpState } = useHttp(addQuote);
 
   const addQuoteHandler = quoteData => {
-    console.log(quoteData);
-
+    sendRequest(quoteData);
     history.push('/quotes');
   };
 
---
+++ src/hooks/use-http.js
@@ -0,0 +1,57 @@
+import { useReducer } from 'react';
+import { useCallback } from 'react';
+
+const SEND_ACTION = 'SEND';
+const SUCCESS_ACTION = 'SUCCESS';
+const ERROR_ACTION = 'ERROR';
+
+const httpReducer = (state, action) => {
+  switch (action.type) {
+    case SEND_ACTION:
+      return {
+        status: 'pending',
+        data: null,
+        error: null,
+      };
+    case SUCCESS_ACTION:
+      return {
+        status: 'completed',
+        data: action.data,
+        error: null,
+      };
+    case ERROR_ACTION:
+      return {
+        status: 'error',
+        data: null,
+        error: action.errMsg,
+      };
+    default:
+      return state;
+  }
+};
+
+export default function useHttp(requestFn) {
+  const [httpState, dispatch] = useReducer(httpReducer, {
+    status: null,
+    data: null,
+    error: null,
+  });
+
+  const sendRequest = useCallback(
+    async requestData => {
+      dispatch({ type: SEND_ACTION });
+      try {
+        const responseData = await requestFn(requestData);
+        dispatch({ type: SUCCESS_ACTION, data: responseData });
+      } catch (err) {
+        dispatch({ type: ERROR_ACTION, errMsg: err.message });
+      }
+    },
+    [requestFn]
+  );
+
+  return {
+    sendRequest,
+    httpState,
+  };
+}
